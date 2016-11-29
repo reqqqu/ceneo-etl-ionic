a1ae7a5d602e406204b5dfc2b2977d90a4eb8267
Rafal Kolasinski <rafal.kolasinski92@gmail.com>
2016-11-29 19:52:18 +0100

    initial project setup

---
+++ app/src/controllers/mainController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name CeneoETL.controller:MainController
+ * @description
+ * # MainController
+ */
+module.exports = [
+    '$scope',
+
+    function( $scope )
+    {
+        // do something with $scope
+    }
+];
