24d1ba7b8bc922ffbc81c3fdb75ff3ae31bbbb8e
Rafal Kolasinski <rafal.kolasinski92@gmail.com>
2016-12-31 17:05:07 +0100

    build 0.2.1 - added CORS workaround in server.js

--- server.js
+++ server.js
@@ -3,12 +3,17 @@
  */
 
 var express = require('express');
+var request = require('request');
 var app = express();
 var server = require('http').createServer(app);
 
 server.listen(process.env.PORT || 3000);
 
-app.use(express.static('public'));
+app.use(express.static('static'))
+  .use('/proxy', function(req, res) {
+    var target = req.url.replace('/?url=','');
+    req.pipe(request(target)).pipe(res);
+  });
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
