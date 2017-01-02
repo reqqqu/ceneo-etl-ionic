7a637ae37ac05bf607e6dd859614a2397fb694b4
Sylwia Grzegolec <s.grzegolec@gmail.com>
2017-01-02 10:52:14 +0100

    Fixed ng-repeat for displaying reviews

--- app/src/services/CSVService.js
+++ app/src/services/CSVService.js
@@ -15,13 +15,13 @@ module.exports = [
       return DBService.getProductWithId(productId).then(function (productFromDB) {
         var reviews = productFromDB.reviews;
         var json1 = JSON.stringify(reviews);
-        var r = new RegExp(";", 'g');
-        var json2 = json1.replace(/\\n/g, "")
-          .replace(/\[\]/g, "\"---\"")
-          .replace(r, "");
+        var r = new RegExp(';', 'g');
+        var json2 = json1.replace(/\\n/g,'')
+          .replace(/\[\]/g, '\'---')
+          .replace(r,'');
         var csv = _convert(json2);
         csv ='ADVANTAGES; DISADVANTAGES; SUMMARY; STARS COUNT; AUTHOR; SUBMISSION DATE; RECOMMENDS PRODUCT; RATED USEFUL COUNT; RATED USELESS COUNT; ID; \n' + csv;
-        var blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
+        var blob = new Blob([csv], { type: 'text/csv;charset=utf-8' });
         var fileName = 'ProductReviews_' + productId + '.csv';
         saveAs(blob, fileName);
       });
@@ -29,14 +29,14 @@ module.exports = [
 
     function saveSingleReviewToTXT(reviewId) {
 
-      var review = "";
+      var review ='';
       for (var count=0; count<=9; count++) {
         var name = Object.getOwnPropertyNames(reviewId)[count];
         var key =  reviewId[Object.keys(reviewId)[count]];
         review = review + name + ': ' + key + '\r\n' + '\r\n';
       }
       var id =  reviewId[Object.keys(reviewId)[9]];
-      var blob = new Blob([review], {type: "text/plain;charset=utf-8"});
+      var blob = new Blob([review], {type: 'text/plain;charset=utf-8'});
       var fileName = 'ReviewId_' + id + '.txt';
       saveAs(blob, fileName);
     };
@@ -46,13 +46,13 @@ module.exports = [
      * @param objArray
      */
     function _convert(objArray) {
-      var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+      var array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
       var str = '';
 
       for (var i = 0; i < array.length; i++) {
         var line = '';
         for (var index in array[i]) {
-          if (line != '') line += ';'
+          if (line !== '') line += ';'
           line += array[i][index];
         }
         str += line + '\r\n';
