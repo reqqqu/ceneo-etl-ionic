62373f053a1dfae81a4ba2eaad0605a45a3fbd77
Rafal Kolasinski <rafal.kolasinski92@gmail.com>
2017-01-02 19:59:40 +0100

    hotfix for error while saving all reviews in release 1.0.0

--- app/src/services/CSVService.js
+++ app/src/services/CSVService.js
@@ -15,14 +15,14 @@ module.exports = [
       return DBService.getProductWithId(productId).then(function (productFromDB) {
         var reviews = productFromDB.reviews;
         var json1 = JSON.stringify(reviews);
-        var r = new RegExp(';', 'g');
-        var json2 = json1.replace(/\\n/g,'')
-          .replace(/\[\]/g, '\'---')
-          .replace(r,'');
+        var r = new RegExp(";", "g");
+        var json2 = json1.replace(/\\n/g, "")
+          .replace(/\[\]/g, "\"---\"")
+          .replace(r, "");
         var csv = _convert(json2);
         csv ='ADVANTAGES; DISADVANTAGES; SUMMARY; STARS COUNT; AUTHOR; SUBMISSION DATE; RECOMMENDS PRODUCT; RATED USEFUL COUNT; RATED USELESS COUNT; ID; \n' + csv;
-        var blob = new Blob([csv], { type: 'text/csv;charset=utf-8' });
-        var fileName = 'ProductReviews_' + productId + '.csv';
+        var blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
+        var fileName = "ProductReviews_" + productId + ".csv";
         saveAs(blob, fileName);
       });
     }
@@ -34,15 +34,15 @@ module.exports = [
      */
     function saveSingleReviewToTXT(reviewObject, productId) {
 
-      var review ='';
+      var review = "";
       for (var count=0; count<=9; count++) {
         var name = Object.getOwnPropertyNames(reviewObject)[count];
         var key =  reviewObject[Object.keys(reviewObject)[count]];
         review = review + name + ': ' + key + '\r\n' + '\r\n';
       }
       var id =  reviewObject.id;
-      var blob = new Blob([review], {type: 'text/plain;charset=utf-8'});
-      var fileName = 'Review_nr_' + id +'_from_product_nr_' + productId +'.txt';
+      var blob = new Blob([review], {type: "text/plain;charset=utf-8"});
+      var fileName = "Review_nr_" + id +"_from_product_nr_" + productId + ".txt";
       saveAs(blob, fileName);
     };
 
@@ -51,6 +51,7 @@ module.exports = [
      * @param objArray
      */
     function _convert(objArray) {
+      console.log(objArray)
       var array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
       var str = '';
 
